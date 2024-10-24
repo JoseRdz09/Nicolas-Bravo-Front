4aee5d62d728d62209f5f7d580fd8e425968dfcb
JoseRdz09 <163470294+JoseRdz09@users.noreply.github.com>
2024-10-24 11:33:57 -0600

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api', // Cambia esto por la URL de tu API
+  baseURL: 'https://kolfipi.mnz.dom.my.id//api', // Cambia esto por la URL de tu API
 });
 
 export default instance;
